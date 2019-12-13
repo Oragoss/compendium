eced42a8dea27b2fdf279d8e456f3fa2948fdce0
Oragoss <interactiveemerald@gmail.com>
2019-12-12 21:48:16 -0600

    New Angular project

--- client/src/Collection/SpecificCollectionContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const SpecificCollectionContext = React.createContext();
-
-export default SpecificCollectionContext;
--- showcase/src/README.md
+++
@@ -1,1 +0,0 @@
-# This project is for the github pages showcase
